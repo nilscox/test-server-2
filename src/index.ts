6885210d334b5e1be3a738efcc2ab8ec887c2243
nilscox <nilscox.dev@gmail.com>
2023-04-13 09:45:21 +0200

    test server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export const hello = (who = 'world') => {
-  return `Hello ${who}!`;
-};
+import { createServer } from 'http';
+
+createServer((req, res) => {
+  res.end();
+}).listen(8854, '0.0.0.0', () => {
+  console.log('server started on port 8854');
+});
